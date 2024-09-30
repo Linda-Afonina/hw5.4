f36914e2e54fca170990437e4938c6400b9b6e46
linda <charming.linda85@gmail.com>
2024-09-30 21:49:54 +0300

    ДЗ 5.4 сделано

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            int result = divide(10, 0);
+            System.out.println("Результат деления: " + result);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Делить на ноль нельзя!");
+        }
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
     }
 }
